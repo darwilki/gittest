ed5fc37b547ec3b839ebb16f2c2675f67d3e7301
darwilki <darwilki@LUK-5CG90578G5.corp.capgemini.com>
2019-09-10 15:34:37 +0100

    Removed DocType line from testng xml to resolve a Maven certification

--- gittest/src/test/java/gittest/gittest/AppTest.java
+++ gittest/src/test/java/gittest/gittest/AppTest.java
@@ -10,13 +10,14 @@ public class AppTest {
 	
 private WebDriver driver;
 
-String eTitle = "Meet Guru99";
+String eTitle = "Meet Guru99 - Free Training Tutorials & Video for IT Courses";
 String aTitle = "";
 
 	@Test
 	public void guru99tutorials() {
 		driver.get("http://www.guru99.com/");
 		aTitle = driver.getTitle();
+		System.out.println(aTitle);
 		
 		if (aTitle.contentEquals(eTitle)) {
 			System.out.println("Test Passed");
