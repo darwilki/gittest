f76d35b60820aa9bf342260f24a25e48463daf9e
darwilki <darwilki@LUK-5CG90578G5.corp.capgemini.com>
2019-09-10 14:39:21 +0100

    POM file updated with dependencies and build information

--- gittest/src/test/java/gittest/gittest/AppTest.java
+++ gittest/src/test/java/gittest/gittest/AppTest.java
@@ -2,7 +2,6 @@ package gittest.gittest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;		
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;	
